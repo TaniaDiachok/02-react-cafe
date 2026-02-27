ea3e22bd439bcf49dbcd4fa6e37f12a6528b485d
Tania Diachok <tetianadiachok14@gmail.com>
2026-02-28 00:39:04 +0100

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,6 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 // Нормалізація стилів
 import "modern-normalize";
-// Глобальні стилі (додатково)
-import "./global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
